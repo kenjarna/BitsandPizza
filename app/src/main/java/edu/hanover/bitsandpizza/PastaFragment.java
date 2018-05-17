c0b0b97fe11f76326104a10006ab100323143e92
Kenny Jarnagin <jarnagink19@hanover.edu>
2018-05-17 11:10:32 -0400

    Initial commit

---
+++ app/src/main/java/edu/hanover/bitsandpizza/PastaFragment.java
@@ -0,0 +1,28 @@
+package edu.hanover.bitsandpizza;
+
+
+import android.os.Bundle;
+import android.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * A simple {@link ListFragment} subclass.
+ */
+public class PastaFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.pasta));
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/edu/hanover/bitsandpizza/StoresFragment.java
@@ -0,0 +1,28 @@
+package edu.hanover.bitsandpizza;
+
+
+import android.os.Bundle;
+import android.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+/**
+ * A simple {@link ListFragment} subclass.
+ */
+public class StoresFragment extends ListFragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.pasta));
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
